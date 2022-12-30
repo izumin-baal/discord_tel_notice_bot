80263a809d5a63fbe1af203749a7cd14ebceb172
izumin-baal <milton96.engineer@gmail.com>
2022-12-31 02:24:58 +0900

    discord.pyの新しいバージョンに対応+調整

--- discord_noti.py
+++ discord_noti.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 # botのトークン
 BOT_TOKEN = "*************************************"
 # サーバーID(int型)
-SERVER_ID = 000000000000
+SERVER_ID = "000000000000"
 # 通知させるチャンネルのID
 ALERT_CHANNEL = 00000000000
 # 通話参加時のみ付与させるロールのID
@@ -13,13 +13,13 @@ ROLE_ID = 00000000
 # 通知を除外させたいメンバーID(Rhythmとか)
 EXCLUDE_ID = 000000000
 
-client = discord.Client()
+client = discord.Client(intents=discord.Intents.all())
 
 @client.event
 async def on_voice_state_update(member, before, after):
     # 通話チャンネルの状態を監視、入退室がトリガー
     if str(member.guild.id) == SERVER_ID and (str(before.channel) != str(after.channel)):
-        now = datetime.utcnow() + timedelta(hours=9)
+        #now = datetime.utcnow() + timedelta(hours=9)
         # メッセージを送るチャンネル
         alert_channel = client.get_channel(ALERT_CHANNEL)
         if member.id != EXCLUDE_ID:
